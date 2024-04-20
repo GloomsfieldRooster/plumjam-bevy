84368bf77d1c01a001b19bc96ebbfa34754fb960
GloomsfieldRooster <rooster@gloomsfield.co>
2024-04-20 14:01:04 -0700

    Write code to render Wisteria sprite to quad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins
+            .set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+
+    asset_server: Res<AssetServer>
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane3d::default().mesh().size(2.841, 5.0)),
+        material: materials.add(StandardMaterial {
+            base_color_texture: Some(asset_server.load("wisteria.png")),
+            unlit: true,
+            alpha_mode: AlphaMode::Blend,
+            ..default()
+        }),
+        transform: Transform::from_rotation(Quat::from_rotation_x(std::f32::consts::FRAC_PI_2)),
+        
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 1.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
